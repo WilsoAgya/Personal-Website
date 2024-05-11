c162f1e289f9ba0d39069a8bd07819f61dcb0320
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-05-11 10:54:43 -0700

    Inital Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Hero from './Hero/Hero.jsx'
+import Nav from './Nav/Nav.jsx'
+
+function App() {
+  
+
+    return(
+    <div>
+        <Nav />
+        <Hero/>
+    </div>
+    )
+
+ 
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import "./Nav.scss";
+
+function Nav(){
+    return(
+    <div>
+        <h1>Hello World Nav</h1>
+    </div>
+    )
+}
+
+export default Nav;
