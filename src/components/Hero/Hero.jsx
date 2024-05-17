1cfed0da5f099dc092d393bc0f2dadfecfdbce5b
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-05-17 13:24:14 -0700

    changed background and added icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,8 @@
 import "/scss/Hero.scss";
 import profilepic from "/images/profilepic.jpg";
+import linkedinlogo from "/images/linkedinlogo.png";
+import email from "/images/email.png";
+import githublogo from "/images/githublogo.png";
 
 function Hero(){
     return(
@@ -7,18 +10,24 @@ function Hero(){
         <div className="container-xl">
             <div className="row">
                 <div className="col-lg-12 col-md-12">
-                    <div className="mb-5 px-4 py-5 mt-5">
+                    <div className="mb-5 px-4 py-lg-5 mt-5">
                         <div className="row">
                             <div className="col-md-6">
-                                <div>
-                                    <h1 className="greeting intro col-md-12">Nice to Meet You!</h1>
-                                    <h1 className="intro">I'm <span className="underline col-md-10">Wilson Agyapong</span></h1>
-                                    <h4 className="col-lg-12 mt-5">An aspiring third-year Computer Science student and aspiring full stack developer based in British Columbia, Canada. 
-                                        Passionate for anything tech-related.</h4>
+                                <div class="text-responsive-center text-responsive-md-center">
+                                    <h1 className="col-md-12 text-responsive-center">Nice to Meet You, I'm</h1>
+                                    <h1 className="intro text-responsive-center"><span className="greeting col-md-10">Wilson Agyapong</span></h1>
+                                    <h5 className="col-lg-12 mt-3 mt-md-5 desc text-responsive-center">A third-year Computer Science student and aspiring full stack developer based in British Columbia, Canada. 
+                                        Passionate for solving real-world problems through tech.</h5>
+                                    <img src ={linkedinlogo} alt="logo for linkedin" class="linkedin text-responsive-center"></img>
+                                    <img src={githublogo} alt="logo for github" class="linkedin mx-3 text-responsive-center"></img>
+                                    <img src={email} alt="logo for github" class="linkedin"></img>
+                                    <div class="mt-2">
+                                    <button type="button" class="btn btn-lg btn-primary px-5 btn-purple-gradient">Resume</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className=" col-lg-6 col-md-6 d-flex justify-content-center align-items-center">
-                                <img src={profilepic} alt="Profile Picture" className="profilepic"></img>
+                            <div className=" col-lg-6 col-md-6 mt-3 text-center order-first order-md-last">
+                                <img src={profilepic} alt="Profile Picture" className="profilepic img-fluid"></img>
                             </div>
                         </div>
                     </div>
