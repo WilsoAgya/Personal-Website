be0e6a498a4ba831fdd8a894db39f6fffca1d6f7
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-06-12 17:54:23 -0700

    Signficant changes to website and implemented cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import "/scss/Hero.scss";
 import linkedinlogo from "/images/linkedinlogo.png";
 import email from "/images/email.png";
-import profilepic from "/images/profilepic.jpg";
+import profilepic from "/images/profilepic.png";
 import githublogo from "/images/githublogo.png";
 
 
@@ -9,14 +9,15 @@ function Hero(){
     return(
     <header>
         <div className="container-xl display-hero">
-            <div className="row">               
-                    <div className="py-lg-5 mt-5">
-                        <div className="row px-lg-5">
+
+            <div className="row mx-lg-5">               
+                    <div className="py-lg-5 mt-5 mx-lg-5">
+                        <div className="row">
                             <div className="col-md-6">
-                                <div class="text-responsive-center text-responsive-md-center">
-                                    <h1 className="col-md-12 text-responsive-center">Nice to Meet You, I'm</h1>
-                                    <h1 className="intro text-responsive-center"><span className="greeting col-md-10">Wilson Agyapong</span></h1>
-                                    <h5 className="col-lg-12 mt-3 mt-md-5 desc text-responsive-center">A third-year Computer Science student and aspiring full stack developer based in British Columbia, Canada. 
+                                <div class="text-responsive-center-hero text-responsive-md-center">
+                                <h1 className="col-md-12 text-responsive-center-hero">Nice to Meet You, I'm</h1>
+                                    <h1 className="intro text-responsive-center-hero"><span className="greeting">Wilson Agyapong</span></h1>
+                                    <h5 className="col-lg-12 mt-3 mt-md-5 desc text-responsive-center-hero">A third-year Computer Science student and aspiring full stack developer based in British Columbia, Canada. 
                                         Passionate for solving problems through tech.</h5>
                                     <div className="mt-5">
                                     <img src ={linkedinlogo} alt="logo for linkedin" class="linkedin text-responsive-center"></img>
@@ -24,13 +25,13 @@ function Hero(){
                                     <img src={email} alt="logo for github" class="linkedin"></img>
                                     </div>
                                     <div class="mt-2">
-                                    <button type="button" class="btn btn-lg btn-primary px-5 btn-purple-gradient">Resume</button>
+                                    <button type="button" class="btn btn-lg btn-primary px-5 custom">Resume</button>
                                     </div>
                                 </div>
                             </div>
                            
                             <div className="col-lg-6 col-md-6 mt-lg-3 order-first order-md-last">
-                                <div className="profilepic"></div>
+                                <img src= {profilepic} className="profilepic"></img>
                            
                             </div>
                         </div>
@@ -41,7 +42,7 @@ function Hero(){
                     </div>
 
 
-     
+                
     
     </header>
   );
