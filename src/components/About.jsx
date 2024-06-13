a89c182c5af381031b619c8b1960562d9a65ee11
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-06-12 20:40:52 -0700

    redesigned about section again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,18 +5,12 @@ import Lottie from "lottie-react";
 function About() {
   return (
     <div className="container overflow-hidden">
-      <div className="row py-lg-auto my-5  mx-lg-5">
-        <div className="col-lg-3 d-flex justify-content-center ">
-          <img src = "/images/wilson.png" 
-            width="200px"
-            
-             
-          />
-        </div>
-        <div className="col">
-          <h1 className="title text-responsive-center">
+      <div className="row mx-lg-5 border">
+        
+      <h1 className="title text-responsive-center">
             ABOUT ME
           </h1>
+        <div className="col-7 d-flex justify-content-center border">
           <h5 className="bio text-responsive-center mx-auto">
             I'm currently a third year Computer Science student studying at
             the University of the Fraser Valley, with a concentration in
@@ -27,9 +21,16 @@ function About() {
             hooked on the idea of being a full-stack software developer. If
             you wish to speak to learn more about me and what I do, feel free
             to send a me a message!
-          </h5>
+          </h5> 
+            
+        </div> 
+        <div className="col-5 border">
+          <img src="images/profilepic2.jpg" className="profilepic" />  
         </div>
+      
+      
       </div>
+     
     </div>
   );
 }
