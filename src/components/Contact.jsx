d832bd141d0ff5035275990dd756b92b8b92222a
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-06-20 19:17:36 -0700

    configured email sending

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,60 +1,87 @@
 import "/scss/Contact.scss";
 import React from "react";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    alert("Message Sent");
+
+    emailjs.sendForm('service_2m3sc0b','template_lahv3ya',e.target,'lgxoigna0TcztW0Lx');
+  }
+    
+
   return (
     <div className="container-xl">
       <div className="row mx-4 py-2">
         <h1 className="title">CONTACT</h1>
         <div className=" col-md-8 mb-3 ">
           <h2 className="bio">Let's get in touch!</h2>
+        <form method="post" onSubmit={sendEmail}>
           <div className="mb-3">
+          
             <input
               type="text"
+              name="from_name"
               className="form-control bg-transparent"
               placeholder="Your Name"
               style={{  color: "#FFFFFF", 
                 borderWidth: "3px", /* Adjust the thickness as needed */
                 borderColor: "#FFFFFF", /* Optional: Change the border color to white */
                 borderStyle: "solid" /* Ensure the border style is solid */ }}
+                required
             />
+            
           </div>
           <div className="mb-3">
+         
             <input
               type="email"
+              name="email_from"
               className="form-control bg-transparent"
               placeholder="Your Email"
               style={{  color: "#FFFFFF", 
                 borderWidth: "3px", /* Adjust the thickness as needed */
                 borderColor: "#FFFFFF", /* Optional: Change the border color to white */
                 borderStyle: "solid" /* Ensure the border style is solid */ }}
+                required
             />
+            
           </div>
           <div className="mb-3">
+         
             <input
               type="text"
               className="form-control bg-transparent"
+              name="subject"
               placeholder="Subject"
               style={{  color: "#FFFFFF", 
                 borderWidth: "3px", /* Adjust the thickness as needed */
                 borderColor: "#FFFFFF", /* Optional: Change the border color to white */
                 borderStyle: "solid" /* Ensure the border style is solid */ }}
+                required
             />
+          
           </div>
           <div className="mb-3">
+          
             <textarea
               className="form-control bg-transparent"
               rows="7"
+              name="message"
               placeholder="Your Message"
               style={{  color: "#FFFFFF", 
                 borderWidth: "3px", /* Adjust the thickness as needed */
                 borderColor: "#FFFFFF", /* Optional: Change the border color to white */
                 borderStyle: "solid" /* Ensure the border style is solid */ }}
+                required
             ></textarea>
+          
           </div>
-          <button type="button" class="btn btn-primary px-5 custom mt-2">
+          <button type="submit" class="btn btn-primary px-5 custom mt-2">
             Submit
           </button>
+        </form>
         </div>
         <div className=" col-md-4">
           <h2 className="bio">Coding Music</h2>
