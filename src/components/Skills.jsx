33b4e3b719f25f3a4ddc753c9d9be4fe75008107
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-07-19 13:50:23 -0700

    testing icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,7 @@ import "/scss/Skills.scss";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import vscode from "/images/vscode.png";
 
 function Skills() {
   useEffect(() => {
@@ -10,7 +11,7 @@ function Skills() {
   const tools = [
     {
       name: "VS Code",
-      image: "/images/vscode.png",
+      image: vscode,
     },
     {
       name: "Git",
