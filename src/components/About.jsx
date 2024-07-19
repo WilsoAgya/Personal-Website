6d6a188691e5fd42d8ffb24d9b3b6bed00f0010a
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-07-19 13:46:28 -0700

    forgot the /

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import "/scss/About.scss";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import profilepic2 from "images/profilepic2.jpg";
+import profilepic2 from "/images/profilepic2.jpg";
 
 function About() {
   useEffect(() => {
